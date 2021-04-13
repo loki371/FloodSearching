3cd0a85bf691142735660eadb4801b04d4c7dcce
xuan-vy-nguyen <xuanvy99@gmail.com>
2021-04-13 17:59:22 +0700

    add exp for jwt

--- services/jwt.py
+++ services/jwt.py
@@ -2,7 +2,7 @@ from passlib.context import CryptContext
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
-
+import time
 import base64
 
 SECRET_KEY = "FloodAppSecretKey"
@@ -20,7 +20,13 @@ def extract_token(token: str = Depends(oauth2_scheme)):
         payload = jwt.decode(token, SECRET_KEY)
         username = payload.get('sub')
         exp = payload.get('exp')
+
         if username is None:
+            print("JWT: Username = null")
+            raise credentials_exception
+
+        if exp < int (time.time()):
+            print("EXP < time")
             raise credentials_exception
 
     except JWTError:
