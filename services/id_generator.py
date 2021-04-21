8ce6a2c253080efe4b5f3d1f3e52908919b87c65
xuan-vy-nguyen <xuanvy99@gmail.com>
2021-04-21 12:43:48 +0700

    add MAX_GEN_ID

--- services/id_generator.py
+++ services/id_generator.py
@@ -1,7 +1,8 @@
 CURRENT_ID = 0
+MAX_GEN_ID = 500
 
 async def generate():
-    global CURRENT_ID
-    CURRENT_ID = CURRENT_ID + 1
+    global CURRENT_ID, MAX_GEN_ID
+    CURRENT_ID = (CURRENT_ID + 1) % MAX_GEN_ID
     print("generateID: CURRENT_ID = ", CURRENT_ID)
     return CURRENT_ID
