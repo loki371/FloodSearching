7360e596af6a7d1dde65a81d3b162eecd54ce2ca
xuan-vy-nguyen <xuanvy99@gmail.com>
2021-06-28 22:38:18 +0700

    add process no image

--- routers/search_image.py
+++ routers/search_image.py
@@ -16,7 +16,7 @@ router_searching = APIRouter(
     tags=["images"]
 )
 
-@router_searching.post("/{name};{longitude};{latitude};{num_person};{ward_id};{phone}", description="Return list of similar registration")
+@router_searching.post("/image/{name};{longitude};{latitude};{num_person};{ward_id};{phone}", description="Return list of similar registration")
 async def searchImage(
         Authorization: Optional[str] = Header(None),
         image: Optional[UploadFile] = File(...),
@@ -107,6 +107,64 @@ async def searchImage(
             differ_point[i] += search_image.get_distance(regis_img, unknown_encoding)
         else:
             differ_point[i] += search_image.getMaxPointImg()
+
+    print('\n')
+    return [{'differ_point': differ_point, 'registrations' : info_regis, 'url_list': url_list}]
+
+@router_searching.post("/noImage/{name};{longitude};{latitude};{num_person};{ward_id};{phone}", description="Return list of similar registration")
+async def searchImage(
+        Authorization: Optional[str] = Header(None),
+        name: Optional[str] = "",
+        longitude: Optional[float] = 0,
+        latitude: Optional[float] = 0,
+        num_person: Optional[int] = 0,
+        ward_id: Optional[str] = 0,
+        phone: Optional[str] = 0,
+    ):
+
+    # try:
+    #     jwt.extract_token(Authorization)
+    # except JWTError:
+    #     raise HTTPException(status_code=401, detail="token is not valid")
+
+    print("searchImage: request info:\n"
+        + f' - name = {name}\n'
+        + f' - longitude = {longitude}\n'
+        + f' - latitude = {latitude}\n'
+        + f' - num_person = {num_person}\n'
+        + f' - ward_id = {ward_id}\n'
+        + f' - phone = {phone}')
+
+    # load from DB
+    registration_list = registration.find_by_ward_id(ward_id)
+
+    # prepare for searching
+    differ_point = {}
+    size_registration_list = len(registration_list)
+    info_regis = {}
+    url_list = {}
+
+    # calculate point
+    for i in range(size_registration_list):
+        print('\n')
+        # init item_regis and differ_point
+        item_regis = registration_list[i]
+
+        info_regis[i] = item_regis
+        differ_point[i] = 0
+
+        # calculate point by GPS
+        differ_point[i] += search_gps.get_distance(
+            longitude, 
+            latitude, 
+            registration_list[i].longitude, 
+            registration_list[i].latitude)
+
+        # calculate point by name
+        differ_point[i] += search_name.get_distance(registration_list[i].name, name)
+
+        # calculate point by image
+        differ_point[i] += search_image.getMaxPointImg()
 
     print('\n')
     return [{'differ_point': differ_point, 'registrations' : info_regis, 'url_list': url_list}]
