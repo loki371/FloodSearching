ac597d84963f51a2a4db5c14c04789382a0739b1
xuan-vy-nguyen <xuanvy99@gmail.com>
2021-07-05 19:41:06 +0700

    change method calculate distance

--- services/search_gps.py
+++ services/search_gps.py
@@ -3,15 +3,12 @@ POINT_GPS_LAT = 100 # 1 vi do = 100km
 POINT_GPS_LONG = 90 # 1 kinh do = 90km
 POINT_GPS = 20 * math.sqrt(2)
 
-def get_distance(long1, lat1, long2, lat2):
 
-    if (long1 * long2 * lat1 * lat2 == 0):
-        print(f'long1 = {long1}; lat1 = {lat1}; long2 = {long2}; lat2 = {lat2}; pointGPS = {point}')
-        return POINT_GPS
-
-    distance = (long1 - long2) * (long1 - long2)
-    distance += (lat1 - lat2) * (lat1 - lat2)
-    distance = math.sqrt(distance)
-    point = POINT_GPS * distance
-    print(f'long1 = {long1}; lat1 = {lat1}; long2 = {long2}; lat2 = {lat2}; distance = {distance}; pointGPS = {point}')
-    return point
+def get_distance(lon1, lat1, lon2, lat2):
+    R = 6378.137 # Radius of earth in KM
+    dLat = lat2 * math.pi / 180 - lat1 * math.pi / 180
+    dLon = lon2 * math.pi / 180 - lon1 * math.pi / 180
+    a = math.sin(dLat/2) * math.sin(dLat/2) + math.cos(lat1 * math.pi / 180) * math.cos(lat2 * math.pi / 180) * math.sin(dLon/2) * math.sin(dLon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d = R * c
+    return d * 1000 # meters
